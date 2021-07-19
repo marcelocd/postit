f7d9bd29b835b9cf4e36478bf96e3eb9bf990f00
Marcelo <marcelocdias93@gmail.com>
2021-07-18 22:46:21 -0300

    Add jquery and i18n

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,8 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '../js/bootstrap_js_files.js'  
+import '../js/bootstrap_js_files.js'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("jquery")
